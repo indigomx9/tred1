7c5100adc4bec84bdfd7ff4f1755bd9042a30b14
indigomx9 <indigomx9@gmail.com>
2019-07-20 22:09:15 -0400

    Updated the Container.

--- src/containers/JTodosConnect.tsx
+++ src/containers/JTodosConnect.tsx
@@ -5,24 +5,47 @@ import { StoreState } from "../reducers/rootReducer";
 
 interface IProps {
     todos: Todo[];
-    fetchTodos(): any;
+    fetchTodos: Function;
+    deleteTodo: typeof deleteTodo;
 };
 
-class JTodos extends React.Component <IProps> {
+interface IState {
+    fetching: boolean;    
+};
+
+class JTodos extends React.Component <IProps, IState> {
+    state = { fetching: false };
+
+    componentDidUpdate(prevProps: IProps): void {
+        if (!prevProps.todos.length && this.props.todos.length) {
+            this.setState({ fetching: false });
+        }
+    };
+
     onButtonClick = (): void => {
         this.props.fetchTodos();
+        this.setState({ fetching: true });
     };
+
+    onTodoClick = (id: number): void => {
+        this.props.deleteTodo(id);
+    };
+
     renderList = (): JSX.Element[] => {
         return this.props.todos.map((todo: Todo) => {
             return (
-                <main key={todo.id}>{todo.title}</main>
+                <main key={todo.id}>
+                    {todo.title}
+                </main>
             );
         })
     };
+
     render() {
         return (
             <React.Fragment>
                 <button onClick={this.onButtonClick}>Fetch</button>
+                {this.state.fetching ? "LOADING" : null}
                 {this.renderList()}
             </React.Fragment>
         );
